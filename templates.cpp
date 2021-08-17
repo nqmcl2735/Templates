a7fcda9caa690ba7ec40fb276fc506736960202c
nqmcl2735 <88897112+nqmcl2735@users.noreply.github.com>
2021-08-17 11:14:52 +0700

    Add files via upload

---
+++ templates.cpp
@@ -0,0 +1,32 @@
+//ngqminh.cl2735
+#include <bits/stdc++.h>
+#define PB push_back
+#define Random(lf, rt) (lf + rand() % (rt - lf + 1))
+#define pii pair <int,int>
+#define vi vector <int>
+#define vii vector <pii>
+#define mpa make_pair
+#define oo INT_MAX
+#define F first
+#define S second
+#define reset(x, i) memset(x, i, sizeof(x))
+#define sz(x) int(x.size())
+#define maxn 100005
+#define MOD
+#define all(x) x.begin(), x.end()
+#define Pi 3.141592653589793238
+#define cosdo(x) cos((x)/180 * Pi)
+#define sindo(x) sin((x)/180 * Pi)
+#define tando(x) tan((x)/180 * Pi)
+#define cotdo(x) 1/tando(x)
+#define Task ""
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(); cout.tie();
+    freopen(Task".inp", "r", stdin);
+    freopen(Task".out", "w", stdout);
+    ;
+}
